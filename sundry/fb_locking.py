4d5ad42ab5c82d110ca8599b4c4dbc768da5c510
james abel <j@abel.co>
2019-02-22 00:14:20 -0800

    improve file based locking and testing

--- sundry/fb_locking.py
+++ sundry/fb_locking.py
@@ -8,30 +8,53 @@ import time
 
 fb_locking_name = 'fb_locking'
 
+lock_char = 'L'
+unlock_char = '_'
+separator_char = ','
+
 
 class FileBasedLocking:
     """
-    Lock some thing, using a file in the local file system.
+    Lock some thing, using a file in the local file system as the lock mechanism.
     """
-    def __init__(self, application_name=None, author_name=None, timeout=30):
+    def __init__(self, application_name=None, author_name=None, timeout=10*60, instance_name=''):
         self.application_name = application_name
         self.author_name = author_name
         self.timeout = timeout  # seconds
-        self.poll_time = 0.1
+        self.instance_name = instance_name
+
+        self.lock_acquired_threshold = 5  # lock file needs to be constant for this number of consecutive reads to be deemed stable
+        self.poll_time = 1.0  # lock file needs to be constant for this time (in seconds) to be deemed stable
+        self.poll_sleep_time = self.poll_time / float(self.lock_acquired_threshold)
+
+        self.last_lock_not_acquired_message = (None, None)
         self.log = logging.getLogger(fb_locking_name)
         self.init_logger()
 
     def init_logger(self):
-        # derived class should generally override this
+        """
+        Derived class should generally override this method.  While this provided method may work for some very simple applications,
+        using the Balsa package (https://pypi.org/project/balsa/) is recommended.
+
+        For example:
+
+            balsa_log = Balsa(application_name, author_name)
+            balsa_log.init_logger()
+
+        """
+
         logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
 
     def get_lock_file_dir(self):
         """
         Return the directory the lock file will be in.  Override this method to change the directory used.
-        For example, use appdirs' user_data_dir() instead of site_data_dir() for user scope (instead of system scope).
 
-        from appdirs import site_data_dir
-        return os.path.join(site_data_dir(self.application_name, self.author_name))
+        For example, use appdirs' user_data_dir() for user scope or site_data_dir() for system scope:
+            from appdirs import site_data_dir
+
+            return os.path.join(site_data_dir(self.application_name, self.author_name))
+                or
+            return os.path.join(user_data_dir(self.application_name, self.author_name))
 
         :return: lock file directory
         """
@@ -51,93 +74,120 @@ class FileBasedLocking:
         """
         return f"{time.time():30.6f}"
 
-    def _write_lock_string(self, f, lock_uuid):
-        s = f"L,{lock_uuid},{self._get_time()}"
-        f.write(s)
-        f.flush()
-        self.log.debug(f"writing {f.name} : {s}")
+    def _get_instance_name(self):
+        """
+        get instance name truncated and padded to a constant number of characters
+        :return: instance name as an exact number of characters
+        """
+        return f"{self.instance_name:40.40}"
+
+    def _write_lock_string(self, file_path, lock_uuid):
+        string_verified = False
+        s = separator_char.join([lock_char, lock_uuid, self._get_time(), self._get_instance_name()])
+        while not string_verified:
+            with open(file_path, 'w') as f:
+                f.write(s)
+                self.log.debug(f"writing {f.name} : {s}")
+            try:
+                with open(file_path) as f:
+                    string_verified = s == f.read()
+            except PermissionError:
+                pass
+            if not string_verified:
+                time.sleep(self.poll_sleep_time)
+
+    def _is_lock_acquired(self, lock_acquired_count):
+        return lock_acquired_count >= self.lock_acquired_threshold
 
     def acquire_lock(self):
         """
         Acquire the lock.  Returns the UUID of this lock if successful, otherwise None.
         :return: Lock UUID or None if failure.
         """
-        lock_acquired = False
+        lock_acquired_count = 0
         lock_uuid = str(uuid4())
         lock_file_path = self.get_lock_file_path()
         makedirs(os.path.dirname(lock_file_path), exist_ok=True)
         timeout_count = self._get_timeout_count()
-        while not lock_acquired and timeout_count > 0:
+        while not self._is_lock_acquired(lock_acquired_count) and timeout_count > 0:
+            time.sleep(self.poll_sleep_time)
             write_lock = False
             try:
                 with open(lock_file_path, 'r+') as f:
                     contents = f.read()
-                    contents_list = contents.split(',')
-                    file_uuid = contents_list[1]
-                    file_age = time.time() - float(contents_list[2])
-                    if contents[0] == '_':
-                        write_lock = True
-                        self.log.info(f"attempting to lock unlocked file {lock_file_path} file_uuid={file_uuid} --> {lock_uuid}")
-                    elif file_age > self.timeout:
-                        write_lock = True
-                        self.log.warning(f'locking timed out file="{lock_file_path}",file_age={file_age},timeout={self.timeout},contents="{contents}"')
+                    if contents is not None and len(contents) >= 1:
+                        contents_list = contents.split(separator_char)
+                        file_uuid = contents_list[1]
+                        file_age = time.time() - float(contents_list[2])
+                        if contents[0] == unlock_char:
+                            write_lock = True
+                            self.log.info(f"{self.instance_name}:attempting to lock unlocked file {lock_file_path} file_uuid={file_uuid} --> {lock_uuid}")
+                        elif file_age > self.timeout:
+                            write_lock = True
+                            self.log.warning(f'{self.instance_name}:locking timed out file="{lock_file_path}",file_age={file_age},timeout={self.timeout},lock_uuid={lock_uuid},contents="{contents}"')
+                        else:
+                            self.log.debug(f"{self.instance_name}:waiting for {lock_file_path}, locked by {file_uuid} , lock age={file_age}")
                     else:
-                        self.log.debug(f"waiting for {lock_file_path}, locked by {file_uuid} , lock age={file_age}")
-                    if write_lock:
-                        f.seek(0)
-                        self._write_lock_string(f, lock_uuid)
+                        self.log.info(f'{self.instance_name}:could not properly read "{lock_file_path}" - got "{contents}"')
             except FileNotFoundError:
-                with open(lock_file_path, 'w') as f:
-                    write_lock = True
-                    self._write_lock_string(f, lock_uuid)
+                write_lock = True
+            if write_lock:
+                self._write_lock_string(lock_file_path, lock_uuid)
 
             # check that the above write actually ended up in the file (it might not)
-            if write_lock:
-                try:
-                    with open(lock_file_path) as f:
-                        file_lock_id = f.read().split(',')[1]
-                        if file_lock_id == lock_uuid:
-                            lock_acquired = True
-                            self.log.info(f"lock acquired {lock_uuid}")
+            try:
+                with open(lock_file_path) as f:
+                    contents = f.read()
+                    if contents is not None and len(contents) > 0:
+                        contents_list = contents.split(separator_char)
+                        file_lock_id = contents_list[1]
+                        if contents_list[0] == lock_char and file_lock_id == lock_uuid:
+                            lock_acquired_count += 1
+                            self.log.info(f"{self.instance_name}:lock acquired {lock_uuid} : lock_acquired_count={lock_acquired_count}")
                         else:
-                            self.log.info(f"lock not acquired since {lock_uuid} != {file_lock_id}")
-                except FileNotFoundError as e:
-                    self.log.warning(f"{lock_file_path} {e}")
-                except IOError as e:
-                    self.log.warning(f"{lock_file_path} {e}")
+                            if self.last_lock_not_acquired_message != (file_lock_id, lock_uuid):
+                                self.log.info(f"{self.instance_name}:lock not acquired since {lock_uuid} != {file_lock_id} : lock_acquired_count={lock_acquired_count}")
+                                self.last_lock_not_acquired_message = (file_lock_id, lock_uuid)
+                            lock_acquired_count = 0
+            except FileNotFoundError as e:
+                self.log.warning(f"{self.instance_name}:{lock_file_path} {e}")
+            except IOError as e:
+                self.log.warning(f"{self.instance_name}:{lock_file_path} {e}")
 
-            if not lock_acquired:
-                time.sleep(self.poll_time)
-            timeout_count -= 0
+            timeout_count -= 1
 
-        if not lock_acquired:
-            self.log.error(f"lock not acquired")
-        return lock_uuid if lock_acquired else None
+        if not self._is_lock_acquired(lock_acquired_count):
+            self.log.error(f"{self.instance_name}:lock not acquired")
+        return lock_uuid if self._is_lock_acquired(lock_acquired_count) else None
 
     def relinquish_lock(self, lock_uuid):
         assert(lock_uuid is not None)
         lock_relinquished = False
         lock_file_path = self.get_lock_file_path()
-        timeout_count = self._get_timeout_count()
-        while not lock_relinquished and timeout_count > 0:
+        timeout_countdown = self._get_timeout_count()
+        while not lock_relinquished and timeout_countdown > 0:
             try:
                 with open(lock_file_path, 'r+') as f:
-                    file_lock_uuid = f.read().split(',')[1]
-                    if file_lock_uuid == lock_uuid:
-                        f.seek(0)
-                        f.write('_')  # signal unlocked with an underscore in character position 0
-                        lock_relinquished = True
-                        self.log.info(f"relinquished {lock_uuid}")
+                    contents = f.read()
+                    if contents is not None and len(contents) > 0:
+                        file_lock_uuid = contents.split(separator_char)[1]
+                        if file_lock_uuid == lock_uuid:
+                            f.seek(0)
+                            f.write(unlock_char)  # signal unlocked with an underscore in character position 0
+                            lock_relinquished = True
+                            self.log.info(f"{self.instance_name}:relinquished {lock_uuid}")
+                        else:
+                            self.log.info(f"{self.instance_name}:looking for {lock_uuid} but got {file_lock_uuid}")
                     else:
-                        self.log.info(f"looking for {lock_uuid} but got {file_lock_uuid}")
+                        self.log.info(f"{self.instance_name}:{lock_file_path} empty")
             except FileNotFoundError as e:
-                self.log.warning(f"{lock_file_path} {e}")
+                self.log.warning(f"{self.instance_name}:{lock_file_path} {e}")
             except IOError as e:
-                self.log.warning(f"{lock_file_path} {e}")
-            timeout_count -= 1
+                self.log.warning(f"{self.instance_name}:{lock_file_path} {e}")
+            timeout_countdown -= 1
             if not lock_relinquished:
-                time.sleep(self.poll_time)
+                time.sleep(self.poll_sleep_time)
         return lock_relinquished
 
     def _get_timeout_count(self):
-        return round(float(self.timeout)/float(self.poll_time))
+        return round(float(self.timeout) / float(self.poll_sleep_time))
--- test_sundry/test_fb_locking.py
+++ test_sundry/test_fb_locking.py
@@ -2,80 +2,171 @@
 import sys
 import time
 import logging
+import os
+import random
 from sundry import FileBasedLocking
-from threading import Event
-from random import uniform
-from multiprocessing import Process, Event
+import multiprocessing.managers
 
-log = logging.getLogger()
+from balsa import Balsa, get_logger
+from pressenter2exit import PressEnter2Exit
+
+application_name = 'test_fb_locking'
+author_name = 'abel'
+
+log = get_logger(application_name)
+
+temp_dir = 'temp'
 
 
-class TestProcess(Process):
+def get_out_file_path():
+    return os.path.join('temp', 'fb_locking_test_out.txt')
+
+
+def append_out_file(s):
+    with open(get_out_file_path(), 'a') as f:
+        f.write(s)
+
+
+def get_inc_file_path():
+    return os.path.join(temp_dir, 'inc_file.txt')
+
+
+def read_work_file(caller_uuid, caller_process_name, caller_count):
+    file_value = None
+    with open(get_inc_file_path()) as f:
+        s = f.read()
+        if s is not None and len(s) > 0:
+            file_value_string, file_uuid, file_process_name = s.split(',')
+            try:
+                file_value = int(file_value_string)
+            except ValueError as e:
+                print(f"{caller_uuid} {e}")
+        else:
+            file_value_string = None
+            file_uuid = None
+            file_process_name = None
+    if file_value is None:
+        sys.exit(caller_uuid)
+
+    s = f"{file_value},r,{file_uuid},{file_process_name},{caller_uuid},{caller_process_name},{caller_count}"
+    append_out_file(f"{s}\n")
+    # print(s)
+
+    return file_value, file_value_string
+
+
+def write_work_file(file_value, test_uuid, process_name, stats):
+
+    s = f"{file_value},w,{test_uuid},{process_name},{stats}"
+    append_out_file(f"{s}\n")
+    # print(s)
+
+    with open(get_inc_file_path(), 'w') as f:
+        int(f.write(f"{file_value},{test_uuid},{process_name}"))
+
+
+class TestProcess(multiprocessing.Process):
 
     class MyFileBasedLocking(FileBasedLocking):
 
         def get_lock_file_dir(self):
-            return "temp"
+            return temp_dir
 
         def init_logger(self):
-            logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
+            balsa_log = Balsa(self.instance_name, author_name, log_directory=os.path.join('temp', 'log'), delete_existing_log_files=True)
+            balsa_log.init_logger()
+            self.log = get_logger(application_name)
 
-    def __init__(self, verbose=False):
+    def __init__(self, iterations, verbose):
         super().__init__()
-        self._exit_event = Event()
         self._start_time = time.time()
+        self.iterations = iterations
         self.verbose = verbose
 
-    def random_wait(self):
-        # every so often delay a small amount of time before the next round
-        wait_time = uniform(0.0, 1.0)
-        self._exit_event.wait(wait_time)
-
-    def print_stats(self, test_count, prefix=''):
-        print(f"{time.time()-self._start_time}:{self.name}:{self.pid}={test_count}")
+    def print_stats(self, test_count, stats):
+        print(f"*** done:{time.time()-self._start_time}:{self.name}:{self.pid}:iterations={test_count}:{stats} ***")
 
     def print(self, s):
         if self.verbose:
             print(s)
 
+    def _stats_to_dict(self, total_time, test_count, min_time, max_time):
+        return {'avg': total_time/float(test_count), 'min': min_time, 'max': max_time}
+
     def run(self):
-        fbl = self.MyFileBasedLocking('test', timeout=5)
+        fbl = self.MyFileBasedLocking('test', instance_name=self.name)
+        fbl.log_name = self.name
         test_count = 0
-        while not self._exit_event.is_set():
+        total_time = 0.0
+        min_time = None
+        max_time = None
+
+        while test_count < self.iterations:
             self.print(f"{self.name} waiting")
+
+            start_time = time.time()
             lock_uuid = fbl.acquire_lock()
+            duration = time.time() - start_time
+            total_time += duration
+            min_time = duration if min_time is None else min(min_time, duration)
+            max_time = duration if max_time is None else max(max_time, duration)
+
             assert (lock_uuid is not None)
-            self.print(f"{self.name} working")
-            self.random_wait()  # the 'work' while locked
-            fbl.relinquish_lock(lock_uuid)
-            self.print(f"{self.name} stopped")
-            self.random_wait()  # the other 'work'
+            self.print(f"{self.name} working ({lock_uuid})")
+            file_test_count, file_uuid = read_work_file(lock_uuid, self.name, test_count)
+            time.sleep(random.uniform(0.0, 1.0))  # work while this process owns the lock
             test_count += 1
-        self.print_stats(test_count)
+            write_work_file(file_test_count + 1, lock_uuid, self.name, self._stats_to_dict(total_time, test_count, min_time, max_time))
+            fbl.relinquish_lock(lock_uuid)
+            time.sleep(random.uniform(0.0, 1.0))  # 'other work' (while this process does NOT own the lock)
+            self.print(f"{self.name} stopped")
+        self.print_stats(test_count, self._stats_to_dict(total_time, test_count, min_time, max_time))
 
-    def request_exit(self):
-        self._exit_event.set()
 
+def test_fb_locking(long_test=False):
 
-def test_fb_locking(run_time=4*60*60):
+    if long_test:
+        overall_time = 30*60  # approximate run time in seconds
+        n_processes = 13
+        iterations = int(round(float(overall_time)/float(n_processes))/2.0)  # div by 2 to get approximately the overall runtime
+        print(f"{iterations} iterations")
+    else:
+        # short test
+        n_processes = 3
+        iterations = 5
 
-    n_processes = 30
     processes = []
+    try:
+        os.remove(get_out_file_path())
+    except FileNotFoundError:
+        pass
 
+    try:
+        os.remove(os.path.join(temp_dir, 'test_fbl.txt'))
+    except FileNotFoundError:
+        pass
+    write_work_file(0, 'INIT', 'INIT', 'INIT')
     for process_number in range(0, n_processes):
-        processes.append(TestProcess())
+        processes.append(TestProcess(iterations, False))
     for p in processes:
         p.start()
 
-    time.sleep(run_time)
-
-    print('**** stopping all ****')
-    for p in processes:
-        p.request_exit()
+    time.sleep(2)  # processes will not run without this (!)
+    press_enter_to_exit = PressEnter2Exit(pre_message=f'press enter to force exit of {application_name}',
+                                          post_message=f'"{application_name}" has been forced to exit ...')
+    print()
+    while press_enter_to_exit.is_alive() and all([p.is_alive() for p in processes]):
+        time.sleep(1)
+    if not press_enter_to_exit.is_alive():
+        for p in processes:
+            p.kill()
     for p in processes:
         p.join()
+    total_iterations, _ = read_work_file('FINAL', 'FINAL', 'FINAL')
+    print(f"iterations : got {total_iterations}, expected {n_processes * iterations}")
+    assert(total_iterations == n_processes * iterations)
 
 
 if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
-    test_fb_locking()
+    test_fb_locking(True)  # long test
