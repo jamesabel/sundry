fb9897e4ee3655ba540f602933b3f33e007f8946
james abel <j@abel.co>
2019-02-25 12:32:49 -0800

    clean up more logic in to_dynamodb add OrderedDict and defaultdict add to_dynamodb test cases

--- sundry/to_dynamodb.py
+++ sundry/to_dynamodb.py
@@ -1,62 +1,26 @@
+
 import decimal
+from collections import OrderedDict, defaultdict
 
 # Handles Inexact error.
 decimal_context = decimal.getcontext().copy()
 decimal_context.prec = 38  # Numbers can have 38 digits precision
 
 
-def dict_to_dynamodb(orig_dict):
+def dict_to_dynamodb(input_value):
     """
     makes a dictionary follow boto3 item standards
-    :param orig_dict: original dictionary
+    :param input_value: input dictionary
     :return: converted version of the original dictionary
     """
-    if type(orig_dict) is dict:
-        resp = {}
-
-        for key, value in orig_dict.items():
-
-            if type(value) is str:
-                resp[key] = value
-
-            elif type(value) is float:
-                resp[key] = decimal_context.create_decimal(value)
-
-            elif type(value) is int:
-                resp[key] = decimal_context.create_decimal(value)
-
-            elif type(value) is dict:
-                resp[key] = dict_to_dynamodb(value)
-
-            elif type(value) is list:
-                resp[key] = []
-                for v in value:
-                    resp[key].append(dict_to_dynamodb(v))
-
-            elif value is None:
-                pass
-
-            else:
-                resp[key] = value
-
-    elif type(orig_dict) is not list:
-        resp = None
-        if type(orig_dict) is str:
-            resp = orig_dict
-
-        elif type(orig_dict) is float:
-            resp = decimal.Decimal(str(orig_dict))
-
-        elif type(orig_dict) is int:
-            resp = decimal.Decimal(str(orig_dict))
-
-        elif orig_dict is None:
-            pass
-
-        else:
-            resp = orig_dict
-
+    if type(input_value) is dict or type(input_value) is OrderedDict or type(input_value) is defaultdict:
+        resp = {k: dict_to_dynamodb(v) for k, v in input_value.items()}
+    elif type(input_value) is list:
+        resp = [dict_to_dynamodb(v) for v in input_value]
+    elif type(input_value) is str or type(input_value) is bool or input_value is None:
+        resp = input_value
+    elif type(input_value) is float or type(input_value) is int:
+        resp = decimal.Decimal(str(input_value))
     else:
-        raise NotImplementedError
-
+        raise NotImplementedError(type(input_value), input_value)
     return resp
--- test_sundry/test_to_dynamodb.py
+++ test_sundry/test_to_dynamodb.py
@@ -1,11 +1,19 @@
 
 import decimal
+from collections import OrderedDict, defaultdict
 from pprint import pprint
 
 from sundry import dict_to_dynamodb
 
 
 def test_dict_to_dynamodb():
+
+    od = OrderedDict()
+    od['a'] = 1
+    od['b'] = 2
+
+    dd = defaultdict(int)
+    dd[1] = 2
 
     sample_input = {
         'sample1': 'Test Data',
@@ -23,7 +31,9 @@ def test_dict_to_dynamodb():
         'sample8': [
             9,
             10
-        ]
+        ],
+        'od': od,
+        'dd': dd
     }
 
     dynamodb_dict = dict_to_dynamodb(sample_input)
@@ -31,7 +41,7 @@ def test_dict_to_dynamodb():
     assert(dynamodb_dict['sample1'] == 'Test Data')
     assert(dynamodb_dict['sample2'] == decimal.Decimal('2'))
     assert(dynamodb_dict['sample3'] is True)
-    assert('sample5' not in dynamodb_dict.keys())
+    assert(dynamodb_dict['sample5'] is None)
     assert(dynamodb_dict['sample6'] == {'test': True})
     assert(dynamodb_dict['sample7'] == ["Hello", "World"])
     assert(dynamodb_dict['sample8'] == [decimal.Decimal(9), decimal.Decimal(10)])
