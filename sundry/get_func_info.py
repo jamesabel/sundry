ce4820ed5b947c9bee7f99dee712598ef3501623
james abel <j@abel.co>
2019-03-07 14:04:38 -0800

    add get_line_number

--- sundry/get_func_info.py
+++ sundry/get_func_info.py
@@ -8,3 +8,11 @@ def get_func_name() -> str:
     :return: name of the calling function
     """
     return inspect.stack()[1].frame.f_code.co_name
+
+
+def get_line_number() -> str:
+    """
+    get name of the *calling* function
+    :return: name of the calling function
+    """
+    return inspect.stack()[1].frame.f_lineno
--- test_sundry/test_get_func_info.py
+++ test_sundry/test_get_func_info.py
@@ -1,10 +1,11 @@
 
-from sundry import get_func_name
+from sundry import get_func_name, get_line_number
 
 
 def test_get_func_name():
     assert(get_func_name() == "test_get_func_name")
-    return get_func_name()
+    assert(get_line_number() == 7)
+    return get_func_name(), get_line_number()
 
 
 if __name__ == "__main__":
