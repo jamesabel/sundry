f17a53cb99a9221aba991d8ae3e59078b2f1c7af
james abel <j@abel.co>
2019-06-04 13:41:03 -0700

    add "on" and "off" to test_type_conversion()

--- test_sundry/test_type_conversion.py
+++ test_sundry/test_type_conversion.py
@@ -5,8 +5,8 @@ from sundry import to_bool
 
 
 def test_type_conversion():
-    test_values = {False: [False, 'F', 'false', 'False', 'FALSE', 'F', 'N', 'no', 0, Decimal(0), Decimal(0.0)],
-                   True: [True, 'T', 'true', 'yes', 'Y', 1, Decimal(1), Decimal(1.0)],
+    test_values = {False: [False, 'F', 'false', 'False', 'FALSE', 'F', 'N', 'no', 0, Decimal(0), Decimal(0.0), 'off'],
+                   True: [True, 'T', 'true', 'yes', 'Y', 1, Decimal(1), Decimal(1.0), 'on'],
                    None: [None, 'None', 'NULL', 'null']}
     for value, variants in test_values.items():
         for variant in variants:
