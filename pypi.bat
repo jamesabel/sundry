63fce93eb22f24fd864e466ba112daf529ccb943
james abel <j@abel.co>
2019-07-09 08:51:59 -0700

    use twine

--- pypi.bat
+++ pypi.bat
@@ -4,4 +4,7 @@ del /Q dist\*.*
 copy /Y LICENSE LICENSE.txt
 copy /Y docs\source\readme.rst readme.rst
 copy /Y docs\source\readme.rst sundry\readme.rst
-venv\Scripts\python.exe setup.py bdist_wheel upload -r pypi
+call venv\Scripts\activate.bat
+python.exe setup.py bdist_wheel
+twine upload dist/*
+deactivate
