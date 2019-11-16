aae19c6f9b9cff5dbd9ea038d7c2e5eedbb0d6a0
James Abel <j@abel.co>
2019-11-16 11:05:31 -0800

    put dict is close in a class

--- sundry/dict_is_close.py
+++ sundry/dict_is_close.py
@@ -1,8 +1,37 @@
 
 import math
+from typing import TypeVar
+
+DictIsCloseNumber = TypeVar('DictIsCloseNumber', int, float, complex)
 
 
-def dict_is_close(x, y, rel_tol=None, abs_tol=None):
+class DictIsClose:
+    def __init__(self, x: (dict, DictIsCloseNumber), y: (dict, DictIsCloseNumber), rel_tol: float = None, abs_tol: float = None):
+        self._x = x
+        self._y = y
+        self._rel_tol = rel_tol
+        self._abs_tol = abs_tol
+
+    def is_close(self):
+        if type(self._x) is float and type(self._y) is float:
+            if self._rel_tol is None and self._abs_tol is None:
+                # default tolerance
+                return math.isclose(self._x, self._y)
+            elif self._rel_tol is not None and self._abs_tol is not None:
+                # user supplied tolerance
+                return math.isclose(self._x, self._y, rel_tol=self._rel_tol, abs_tol=self._abs_tol)
+            else:
+                # require both
+                raise ValueError(self._rel_tol, self._abs_tol)
+        elif type(self._x) is dict and type(self._y) is dict:
+            if self._x.keys() != self._y.keys():
+                return False
+            return all([DictIsClose(self._x[k], self._y[k], self._rel_tol, self._abs_tol).is_close() for k in self._x])
+        else:
+            return self._x == self._y
+
+
+def dict_is_close(x: (dict, DictIsCloseNumber), y: (dict, DictIsCloseNumber), rel_tol: float = None, abs_tol: float = None):
     """
 
     Like doing x == y for a dict, except if there are floats then use math.isclose()
@@ -18,19 +47,5 @@ def dict_is_close(x, y, rel_tol=None, abs_tol=None):
     :return: True if dictionaries match and float values are close
 
     """
-    if type(x) is float and type(y) is float:
-        if rel_tol is None and abs_tol is None:
-            # default tolerance
-            return math.isclose(x, y)
-        elif rel_tol is not None and abs_tol is not None:
-            # user supplied tolerance
-            return math.isclose(x, y, rel_tol=rel_tol, abs_tol=abs_tol)
-        else:
-            # require both
-            raise ValueError(rel_tol, abs_tol)
-    elif type(x) is dict and type(y) is dict:
-        if x.keys() != y.keys():
-            return False
-        return all([dict_is_close(x[k], y[k], rel_tol, abs_tol) for k in x])
-    else:
-        return x == y
+    _dict_is_close = DictIsClose(x, y, rel_tol, abs_tol)
+    return _dict_is_close.is_close()
