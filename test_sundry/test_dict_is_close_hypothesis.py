7f439ae64c17c0b8ddf0bd0231e110e46c9fb3c8
James Abel <j@abel.co>
2019-11-17 09:36:21 -0800

    add hypothesis testing

--- test_sundry/test_dict_is_close_hypothesis.py
+++ test_sundry/test_dict_is_close_hypothesis.py
@@ -1,6 +1,22 @@
+import math
 
 from hypothesis import given
-from hypothesis.strategies import text
+from hypothesis.strategies import floats
+from ismain import is_main
 
-def test_dict_is_close_hyposthesis():
-    pass
+from sundry import dict_is_close
+
+
+@given(floats(), floats(), floats(), floats())
+def tst_dict_is_close_hypothesis(x, y, r, a):
+    r = abs(r)
+    a = abs(a)
+    assert dict_is_close(x, y,  rel_tol=r, abs_tol=a) == math.isclose(x, y, rel_tol=r, abs_tol=a)
+
+
+def test_dict_is_close_hypothesis():
+    tst_dict_is_close_hypothesis()
+
+
+if is_main():
+    test_dict_is_close_hypothesis()
